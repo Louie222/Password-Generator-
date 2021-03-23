fde2975a2888f338ead8f9cdf638aa8cd2fd6553
Louie Obaldo <louie.obaldo@education.wa.edu.au>
2021-03-23 14:03:44 +0800

    Updated my notes on the Javascript page

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -13,59 +13,64 @@ function writePassword() {
 
 }
 
-// At this stage alerts will pop up for the user to validate the password criteria. It will return a random secure password for the function generatePassword  
-function generatePassword(){
-  var numbers = "0123456789"; 
+// The function generatePassword has been invoked with different variables that represent different values. These are contained in strings.   
+function generatePassword() {
+  var numbers = "0123456789";
   var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
   var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var symbols = "!#$%&'()*+,-./:;<=>?@[^_`]{|}~\ ";
-  var chars = ''; 
-  var password1 = '';  
-//Making sure that the generated password has no issues and is generating according to the length set, which is a substring 
-  console.log(chars.substring(7, 10));
-  console.log (chars.length);
-
+  console.log(symbols)
+  var Chars = "";
+  var password1 = "";
   var passwordLength = prompt("Please enter between 8 and 128 characters for your password length");
 
-//This is validating the users password length
-  if (passwordLength === null){}
-  else if (passwordLength == '' || parseInt(passwordLength) < 8 || parseInt(passwordLength) > 128){
+  //This is validating the users password length
+  if (passwordLength === null) { }
+  //I have used the parseInt function because passwordLength value = null is a string argument, which returns an integer. The integers in this case are from 8-128.   
+  else if (passwordLength == "" || parseInt(passwordLength) < 8 || parseInt(passwordLength) > 128) {
     alert("Invalid Password Length. Please follow length criteria");
   }
-// At this stage this is allowing users to include character type in their password
-var numberResponse = confirm("Do you want numbers in your password?");
-if (numberResponse){
-  chars = chars + numbers;
+  // At this stage this is prompting users to include numbers in their password
+  var numberResponsePrompt = confirm("Do you want numbers in your password?");
+  if (numberResponsePrompt) {
+    Chars = Chars + numbers;
 
-}
+  }
+// At this stage this is prompting users to include lowercase characters in their password
+  var lowerCaseCharsPrompt = confirm("Do you want lowercase characters in your password?");
+  if (lowerCaseCharsPrompt) {
+    Chars = Chars + lowerCaseChars;
 
-var lowerCaseChars = confirm("Do you want lowercase characters in your password?");
-if (lowerCaseChars){
-  chars = chars + lowerCaseChars;
+  }
 
-}
+// At this stage this is prompting users to include uppercase characters in their password
+  var upperCaseCharsPrompt = confirm("Do you want uppercase characters in your password?");
+  if (upperCaseCharsPrompt) {
+    Chars = Chars + upperCaseChars;
 
-var upperCaseChars = confirm("Do you want uppercase characters in your password?"); 
-if (upperCaseChars){
-  chars = chars + upperCaseChars;
+  }
 
-}
+// At this stage this is prompting users to include symbols in their password
+  var symbolPrompt = confirm("Do you want symbols in your password?");
+  if (symbolPrompt) {
+    Chars = Chars + symbols;
+    console.log(symbols)
 
-var symbols = confirm("Do you want symbols in your password?"); 
-if (symbols){
-  chars = chars + symbols;
+  }
 
-}
+//Here is the statement that makes sure users are selecting at least one character type in their password.
+//If they don't input at least one character type an alert will pop up stating that    
 
-if (chars != ''){
-  for (var i=0; i<passwordLength; i++){
-    var randomnum = Math.floor(Math.random() * chars.length);
-    password1 += chars.substring(randomnum,randomnum+1);
-    console.log (password1);
+  if (Chars != "") {
+    for (var i = 0; i < passwordLength; i++) {
+      var randomnum = Math.floor(Math.random() * Chars.length);
+      password1 += Chars.substring(randomnum, randomnum + 1);
+    }
+    }
+    else{
+      alert("Please select at least one character type");
+    }
+    return password1;
 
-}
-
-return password1; 
-
-}
-}
+   }
+    
